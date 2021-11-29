958889f2256c0e63524925cd817b9eac12d79f0a
devmateusramos <devmateusramos@gmail.com>
2021-11-29 17:11:12 -0300

    babel webpack config

--- src/index.jsx
+++ src/index.jsx
@@ -2,5 +2,6 @@ const user = {
   name: 'Mateus',
 };
 
-console.log(user.address?.street);
-// Para gerar o bundle com o babel yarn babel src/index.js --out-file dist/bundle.js
+function Wrapper() {
+  return <App />;
+}
